dc24e39d3f61590180b5937a0ef77a1a1fae6626
Alle024gg <alle024ggnanana@gmail.com>
2023-11-25 23:35:47 +0200

    update title with js

--- examples.js
+++ examples.js
@@ -1,20 +1,28 @@
-console.info(2 + 3);
-console.warn("Alexandra");
+function variablesExamples() {
+  console.info(2 + 3);
+  console.warn("Alexandra");
 
-var employed = true;
-var age = 18;
-console.log("angajat:", employed);
-employed = false;
-console.log("angajat:", employed);
-employed = "da";
-console.log("angajat:", employed);
+  var employed = true;
+  var age = 18;
+  console.log("angajat:", employed);
+  employed = false;
+  console.log("angajat:", employed);
+  employed = "da";
+  console.log("angajat:", employed);
 
-var skills = ["html", "css"];
-skills;
+  var skills = ["html", "css"];
 
-typeof skills;
-console.debug("tipul variabilei skills", typeof skills);
+  typeof skills;
+  console.debug("tipul variabilei skills", typeof skills);
+}
 
-var job = document.getElementById("job-title");
-console.warn("job:", job);
-console.info(typeof job);
+function updateTitle() {
+  var job = document.getElementById("job-title");
+  console.warn("job:", job, title);
+  console.info(typeof job);
+
+  job.innerHTML = title;
+}
+variablesExamples();
+
+updateTitle("Developer");
