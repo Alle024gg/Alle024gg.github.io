faca1d0444d3409bfab67f44a0900b8e1154f055
Alle024gg <alle024ggnanana@gmail.com>
2023-11-27 21:14:15 +0200

    hide old page

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function showlanguages() {
   // afiseaza pagina languages
+  var oldPage = document.getElementById("projects");
+  oldPage.style.display = "none";
+
   var page = document.getElementById("languages");
   page.style.display = "block";
 
