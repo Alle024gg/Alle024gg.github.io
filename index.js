fb48ab2224d72020d266ab59e3c7bac92733f09c
Alle024gg <alle024ggnanana@gmail.com>
2023-11-28 15:11:43 +0200

    show all pages using menu

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-function showSkills() {
-  var oldPage = document.getElementById("projects");
-  oldPage.style.display = "none";
+function showHOME() {
+  document.getElementById("Skills").style.display = "none";
+  document.getElementById("Projects").style.display = "none";
+  document.getElementById("Languages").style.display = "none";
 
-  var page = document.getElementById("skills");
+  var page = document.getElementById("HOME");
   page.style.display = "block";
 }
 
-function showlanguages() {
-  var oldPage = document.getElementById("projects");
-  oldPage.style.display = "none";
+function showSkills() {
+  document.getElementById("HOME").style.display = "none";
+  document.getElementById("Projects").style.display = "none";
+  document.getElementById("Languages").style.display = "none";
 
-  var page = document.getElementById("languages");
+  var page = document.getElementById("Skills");
+  page.style.display = "block";
+}
+
+function showProjects() {
+  document.getElementById("HOME").style.display = "none";
+  document.getElementById("Skills").style.display = "none";
+  document.getElementById("Languages").style.display = "none";
+
+  var page = document.getElementById("Projects");
+  page.style.display = "block";
+}
+
+function showLanguages() {
+  document.getElementById("HOME").style.display = "none";
+  document.getElementById("Skills").style.display = "none";
+  document.getElementById("Projects").style.display = "none";
+
+  var page = document.getElementById("Languages");
   page.style.display = "block";
 }
---
+++ showHome.js
@@ -0,0 +1,8 @@
+function showHome() {
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+
+  var page = document.getElementById("home");
+  page.style.display = "block";
+}
