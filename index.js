65992456a1f4bd8ab6ba228f295efc1bba7090ca
Alle024gg <alle024ggnanana@gmail.com>
2023-12-04 19:54:09 +0200

    easy hide elements

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
+function hide(id) {
+  console.info("hide", id);
+  document.getElementById(id).style.display = "none";
+}
+
 function showHOME() {
-  document.getElementById("Skills").style.display = "none";
-  document.getElementById("Projects").style.display = "none";
-  document.getElementById("Languages").style.display = "none";
+  hide("skills");
+  hide("Projects");
+  hide("Languages");
 
   var page = document.getElementById("HOME");
   page.style.display = "block";
 }
 
 function showSkills() {
-  document.getElementById("HOME").style.display = "none";
-  document.getElementById("Projects").style.display = "none";
-  document.getElementById("Languages").style.display = "none";
+  hide("HOME");
+  hide("Projects");
+  hide("Languages");
 
   var page = document.getElementById("Skills");
   page.style.display = "block";
 }
 
 function showProjects() {
-  document.getElementById("HOME").style.display = "none";
-  document.getElementById("Skills").style.display = "none";
-  document.getElementById("Languages").style.display = "none";
+  hide("HOME");
+  hide("Skills");
+  hide("Languages");
 
   var page = document.getElementById("Projects");
   page.style.display = "block";
 }
 
 function showLanguages() {
-  document.getElementById("HOME").style.display = "none";
-  document.getElementById("Skills").style.display = "none";
-  document.getElementById("Projects").style.display = "none";
+  hide("HOME");
+  hide("Skills");
+  hide("Projects");
 
   var page = document.getElementById("Languages");
   page.style.display = "block";
